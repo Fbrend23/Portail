8b85b8d64090637c7b5cc015162035c17af6144e
Fbrend23 <158816892+Fbrend23@users.noreply.github.com>
2025-04-21 19:45:45 +0200

    feat(Page)Passage en content "hero"

--- js/scripts.js
+++ js/scripts.js
@@ -62,3 +62,20 @@ document.getElementById("toggle-social").addEventListener("click", function () {
       });
   });
   
+  document.querySelector(".scroll-down").addEventListener("click", () => {
+    document.querySelector("#cards-section").scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+
+  const cards = document.querySelectorAll('.card');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, { threshold: 0.1 });
+
+cards.forEach(card => observer.observe(card));
