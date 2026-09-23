d80935878be82455204b584b2ed0c42f4d639fc4
Brendan Fleurdelys <fbrend23@gmail.com>
2026-09-23 14:55:51 +0900

    chore(deps): retrait de pinia, vanilla-tilt et sass, inutilisés
    Aucun store Pinia, aucun appel à VanillaTilt et aucun style SCSS dans src/.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import { ViteSSG } from 'vite-ssg/single-page'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 
 // Le HTML est pré-généré au build (vite-ssg) pour que le contenu soit lisible
 // sans JavaScript : moteurs de recherche et aperçus de liens.
 export const createApp = ViteSSG(App, async ({ app, isClient }) => {
-  app.use(createPinia())
-
   // Les particules dessinent dans un canvas : uniquement côté navigateur
   if (isClient) {
     const { default: Particles } = await import('@tsparticles/vue3')
